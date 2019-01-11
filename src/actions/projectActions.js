2d878a8bbcdc366fe905d4b2e81ac48cbd5f142a
serhii.borovyk <serhii.borovyk@globallogic.com>
2019-01-11 19:56:00 +0200

    remove proxy from package.json (NOTE: proxies don't work with AXIOS)

--- src/actions/projectActions.js
+++ src/actions/projectActions.js
@@ -1,9 +1,12 @@
 import axios from 'axios';
 import {GET_ERRORS} from "./types";
 
+const proxy = "http://localhost:8080/api";
+
+
 export const createProject = (project, history) => async dispatch => {
     try {
-        const res = await axios.post("http://localhost:8080/api/project", project);
+        const res = await axios.post(proxy + "/project", project);
         console.log(res.data);
         history.push("/dashboard")
     } catch (err) {
