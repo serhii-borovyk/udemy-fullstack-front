05c6cbf2adab7032bc05d7e5962efef0d5cac861
serhii.borovyk <serhii.borovyk@globallogic.com>
2019-01-22 17:06:27 +0200

    add project item boilerplate

--- src/actions/types.js
+++ src/actions/types.js
@@ -4,4 +4,8 @@ export const GET_PROJECT = "GET_PROJECT";
 export const DELETE_PROJECT = "DELETE_PROJECT";
 export const ON_INPUT_CHANGE = "ON_INPUT_CHANGE";
 
+export const GET_BACKLOG = "GET_BACKLOG";
+export const GET_PROJECT_TASK = "GET_PROJECT_TASK";
+export const DELETE_PROJECT_TASK = "DELETE_PROJECT_TASK";
+
 
--- src/components/project/ProjectItem.js
+++ src/components/project/ProjectItem.js
@@ -20,7 +20,7 @@ class ProjectItem extends Component {
                             </div>
                             <div className="col-md-4 d-lg-block">
                                 <ul className="list-group">
-                                    <Link to="#top">
+                                    <Link to={`/projectBoard/${project.projectIdentifier}`}>
                                         <li className="list-group-item board">
                                             <i className="fa fa-flag-checkered pr-1"> Project Board </i>
                                         </li>
